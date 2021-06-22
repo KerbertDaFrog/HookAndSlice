f2626e0d3c3be2a3d0d81eb3a4524c4ed83ca9a4
Jet Yong <jettlag2000@gmail.com>
2021-06-22 15:04:29 +1200

    PlayerLook&MovementImplemented

---
+++ Hook&Slice/Assets/Scripts/Player/PlayerLook.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    [SerializeField]
+    private float sensX;
+    [SerializeField]
+    private float sensY;
+
+    Camera cam;
+
+    private float mouseX;
+    private float mouseY;
+
+    private float multi = 0.01f;
+
+    private float xRot;
+    private float yRot;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        cam = GetComponentInChildren<Camera>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        PlayerInput();
+
+        cam.transform.localRotation = Quaternion.Euler(xRot, 0, 0);
+        transform.rotation = Quaternion.Euler(0, yRot, 0);
+    }
+
+    private void PlayerInput()
+    {
+        mouseX = Input.GetAxisRaw("Mouse X");
+        mouseY = Input.GetAxisRaw("Mouse Y");
+
+        yRot += mouseX * sensX * multi;
+        xRot -= mouseY * sensY * multi;
+
+        xRot = Mathf.Clamp(xRot, -90f, 90f);
+    }
+}
---
+++ Hook&Slice/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField]
+    private GameObject player;
+
+    [SerializeField]
+    private bool isMoving = false;
+    [SerializeField]
+    private bool isSprinting = false;
+    [SerializeField]
+    private bool isGrounded = true;
+    [SerializeField]
+    private float moveSpeed = 6f;
+    [SerializeField]
+    private float movementMultiplier = 10f;
+    [SerializeField]
+    private float jumpHeight = 2f;
+    [SerializeField]
+    private float rbDrag = 6f;
+
+    private float horMov;
+    private float verMov;
+
+    private LayerMask groundMask;
+
+    private Rigidbody playerRB;
+
+    private Vector3 moveDir;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        playerRB.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        PlayerInput();
+        ControlDrag();
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void PlayerInput()
+    {
+        horMov = Input.GetAxisRaw("Horizontal");
+        verMov = Input.GetAxisRaw("Vertical");
+
+        moveDir = transform.forward * verMov + transform.right * horMov;
+    }
+
+    private void MovePlayer()
+    {
+        playerRB.AddForce(moveDir.normalized * moveSpeed * movementMultiplier, ForceMode.Acceleration);
+    }
+
+    private void ControlDrag()
+    {
+        playerRB.drag = rbDrag;
+    }
+
+}
