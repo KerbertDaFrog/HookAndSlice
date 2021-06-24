1351cf08b80c4b69e072f0fc4f8d9e9b19668c19
Jet Yong <jettlag2000@gmail.com>
2021-06-24 15:22:46 +1200

    AddedMoreFoldersForBetterOrganization

---
+++ Hook&Slice/Assets/Scripts/Weapons/Harpoon.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Harpoon : MonoBehaviour
+{
+    [Header("Harpoon Modifiers")]
+    private float damage;
+    private float speed;
+    private float length;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            FireHarpoon();
+        }
+    }
+
+    private void FireHarpoon()
+    {
+
+    }
+}
