f3191ab3d841d93bb9ff43266672bc153f1669d0
ARK <theonlypurplecat@gmail.com>
2021-10-14 13:21:02 +1300

    clearing out some testing scripts and beginning to start UIManager

---
+++ Hook&Slice/Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+
+    private static UIManager _instance;
+
+    public static UIManager Instance { get { return _instance; } }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        _instance = null;
+    }
+
+
+
+
+}
