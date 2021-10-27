304bc7fdc5888d4c5dd6e8cdf322ae08a402e586
ARK <theonlypurplecat@gmail.com>
2021-10-27 19:31:49 +1300

    mage fireball

--- Hook&Slice/Assets/Scripts/Enemies/EnemyRanged.cs
+++ Hook&Slice/Assets/Scripts/Enemies/EnemyRanged.cs
@@ -4,9 +4,59 @@ using UnityEngine;
 
 public class EnemyRanged : Enemy
 {
+    [Header("Fireball")]
+    [SerializeField]
+    private GameObject fireballEmmiter;
+
+    [SerializeField]
+    private GameObject fireball;
+
+    [SerializeField]
+    private float fireballForce;
+
+    private bool attack;
+
+    private bool alreadyAttacked;
+
+
     protected override void GoToAttackState()
     {
         base.Attack();
+        attack = true;
+        //StartCoroutine(FireballDelay());
 
     }
+
+    protected override void LeaveAttackState()
+    {
+        attack = false;
+    }
+
+    //IEnumerator FireballDelay()
+    //{
+    //    if(attack == true)
+    //    {
+    //        //GameObject TemporaryFireballHolder;
+    //        //TemporaryFireballHolder = Instantiate(fireball, fireballEmmiter.transform.position, fireballEmmiter.transform.rotation) as GameObject;
+
+    //        //Rigidbody TemporaryRidgidbody;
+    //        //TemporaryRidgidbody = TemporaryFireballHolder.GetComponent<Rigidbody>();
+
+    //        //TemporaryRidgidbody.AddForce(transform.forward * fireballForce);
+
+    //        //Destroy(TemporaryFireballHolder, 5f);
+    //        yield return new WaitForSeconds(1f);
+    //        if(!alreadyAttacked)
+    //        {
+    //            alreadyAttacked = true;
+    //            Instantiate(fireball, fireballEmmiter.transform.position, fireballEmmiter.transform.rotation);
+    //            yield return new WaitForSeconds(4f);
+    //            alreadyAttacked = false;
+
+    //        }
+
+    //    }
+
+    //}
+
 }
---
+++ Hook&Slice/Assets/Scripts/Enemies/MageAttack.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MageAttack : MonoBehaviour
+{
+    [Header("Fireball")]
+    [SerializeField]
+    private GameObject fireballEmmiter;
+
+    [SerializeField]
+    private GameObject fireball;
+
+    [SerializeField]
+    private float fireballForce;
+
+    public void SpawnFireball()
+    {
+        Instantiate(fireball, fireballEmmiter.transform.position, fireballEmmiter.transform.rotation);
+    }
+
+
+}
