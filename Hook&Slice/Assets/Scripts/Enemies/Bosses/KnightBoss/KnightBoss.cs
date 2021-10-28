24fcffcf64daab223a1ac54328e0936ed3e9872e
Jet Yong <jettlag2000@gmail.com>
2021-10-28 20:47:45 +1300

    KnightBossShockwaveWIP

---
+++ Hook&Slice/Assets/Scripts/Enemies/Bosses/KnightBoss/KnightBoss.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightBoss : Enemy
+{
+    public List<GameObject> armourPieces = new List<GameObject>();
+
+    //[Header("ArmourPieces")]
+    //public GameObject[] armourPieces { get; set; }
+
+    private int shockWavDam;
+
+    public int armourAmount;
+
+    public enum AttackState
+    {
+        nil,
+        slam,
+        range,
+        summon
+    }
+
+    private AttackState currentAttackState;
+
+    protected override void Start()
+    {
+        //armourAmount = armourPieces.Length;
+        
+        base.Start();
+    }
+
+    #region ArmourPieces
+    //private GameObject[] ArmourPieces
+    //{
+    //    get { return armourPieces; }
+    //    set
+    //    {
+    //        if (armourPieces == null)
+    //        {
+    //            armourPieces = value;
+    //            return;
+    //        }
+
+    //        if (armourPieces.Length > value.Length)
+    //        {
+    //            //A GameObject has been destroyed
+
+    //            //Check which GameObject has been destroyed
+    //            for(int i = 0; i < armourPieces.Length; ++i)
+    //            {
+    //                for(int j = 0; j < value.Length; ++j)
+    //                {
+    //                    //Check if GameObject is in the new array
+    //                    if(armourPieces[i].GetInstanceID() == value[j].GetInstanceID())
+    //                    {
+    //                        //GameObject still here
+    //                        continue;
+    //                    }
+    //                }
+    //                //If this part of the code is reached, a destroyed GameObject has been found
+    //                Debug.Log("The GameObject #" + i + "has been destroyed!");
+    //            }
+    //        }
+    //        else if(armourPieces.Length < value.Length)
+    //        {
+    //            //A GameObject has been added
+    //        }
+    //        armourPieces = value;
+    //    }   
+    //}
+    #endregion
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    protected override void EnemyBehaviour()
+    {
+        base.EnemyBehaviour();
+    }
+
+    public int ShockwaveDamage()
+    {
+        return shockWavDam;
+    }
+
+    protected void Attack()
+    {
+
+    }
+
+    IEnumerator SlamAttack()
+    {
+        currentAttackState = AttackState.slam;
+        yield return new WaitForSeconds(0.1f);
+        //slamattack animation
+        //Instantiate shockwave
+    }
+
+    private void RangeAttack()
+    {
+        currentAttackState = AttackState.range;
+        //rangeattack animation
+    }
+
+    private void SummonMinions()
+    {
+        currentAttackState = AttackState.summon;
+        
+        //summon animation
+    }
+
+    protected override void GoToFrenzyState()
+    {
+        base.GoToFrenzyState();
+        StartCoroutine("Frenzy");
+    }
+
+    IEnumerator Frenzy()
+    {
+        StartCoroutine(SlamAttack());
+        while (currentAttackState == AttackState.slam && currentState != EnemyStates.dead)
+        {
+            yield return null;
+        }
+        RangeAttack();
+        while(currentAttackState == AttackState.range && currentState != EnemyStates.dead)
+        {
+            yield return null;
+        }
+        SummonMinions();
+        while(currentAttackState == AttackState.summon && currentState != EnemyStates.dead)
+        {
+            yield return null;
+        }
+    }
+}
---
+++ Hook&Slice/Assets/Scripts/Enemies/Bosses/KnightBoss/Shockwave.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shockwave : MonoBehaviour
+{
+    private KnightBoss damage;
+
+    private void Start()
+    {
+        damage = GetComponent<KnightBoss>();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponentInParent<PlayerHealth>().TakeDamage(damage.Damage());
+        }
+    }
+}
