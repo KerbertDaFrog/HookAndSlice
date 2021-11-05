3c5fb9dc383fc7643aa2f902940ae8863c6e675f
ARK <theonlypurplecat@gmail.com>
2021-11-05 17:45:44 +1300

    Hud-updated

---
+++ Hook&Slice/Assets/Scripts/RandomAnimationStart.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAnimationStart : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        Animator anim = GetComponent<Animator>();
+        //AnimationStateInfo state = anim.GetCurrentAnimatorStateInfo(0);
+        //anim.Play(state.fullPathHash, -1, Random.Range(0f, 1f));
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
