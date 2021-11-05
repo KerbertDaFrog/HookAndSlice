e7b4f09142c48fbddd38be0ca08627dda34cfea3
ARK <theonlypurplecat@gmail.com>
2021-11-05 18:01:08 +1300

    bodged together randomized start to torch animation

--- Hook&Slice/Assets/Scripts/RandomAnimationStart.cs
+++ Hook&Slice/Assets/Scripts/RandomAnimationStart.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class RandomAnimationStart : MonoBehaviour
 {
+    [SerializeField]
+    private Animator anim;
+
+    private void Start()
+    {
+        Invoke("EnableAnimator", Random.value);
+    }
 
     private void Awake()
     {
-        Animator anim = GetComponent<Animator>();
-        //AnimationStateInfo state = anim.GetCurrentAnimatorStateInfo(0);
-        //anim.Play(state.fullPathHash, -1, Random.Range(0f, 1f));
-        
+        //Animator anim = GetComponent<Animator>();
+        //AnimationState state = anim.GetCurrentAnimatorStateInfo(0);
+        //anim.Play(state.full, -1, Random.Range(0f, 1f));
+        //Animator.Play("Torch", 0, Random.value);
     }
 
     // Update is called once per frame
@@ -18,4 +26,10 @@ public class RandomAnimationStart : MonoBehaviour
     {
         
     }
+
+    private void EnableAnimator()
+    {
+        anim.enabled = true;
+    }
+
 }
