bf3629581c80c0e43c088a7fbe28346a0d69fcb2
ARK <theonlypurplecat@gmail.com>
2021-11-16 21:12:49 +1300

    boss things

---
+++ Hook&Slice/Assets/Scripts/Enemies/Bosses/KnightBoss/ArmourFalling.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmourFalling : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject armourSprite;
+
+    public void GroundHit()
+    {
+        //Audio Clip goes here
+        armourSprite.SetActive(true);
+        Destroy(gameObject);
+    }
+}
