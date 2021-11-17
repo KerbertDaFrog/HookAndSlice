0f9e00132e4f499a6d1cdb8389475f6b0f6028ac
ARK <theonlypurplecat@gmail.com>
2021-11-17 16:29:26 +1300

    Boss-room doors removed (until they can be coded properly)

--- Hook&Slice/Assets/Scripts/Level/Enviroment/DoorLocked.cs
+++ Hook&Slice/Assets/Scripts/Level/Enviroment/DoorLocked.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class DoorLocked : Door
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private void OnTriggerExit(Collider other)
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
