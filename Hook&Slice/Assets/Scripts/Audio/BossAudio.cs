07384fd935aade9d064ca3a52aaf4845eb06b6f9
ARK <theonlypurplecat@gmail.com>
2021-11-18 22:02:07 +1300

    attempting to start on boss audio (badly)

---
+++ Hook&Slice/Assets/Scripts/Audio/BossAudio.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAudio : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource swing;
+
+    
+    private void SwingSound()
+    {
+        swing.Play();
+    }
+
+
+
+}
---
+++ Hook&Slice/Assets/Scripts/Audio/PlayOneSound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayOneSound : MonoBehaviour
+{
+
+    [SerializeField]
+    private string audioClipName;
+
+    private void PlaySound()
+    {
+        AudioManager.instance.Play(audioClipName);
+    }
+
+}
