da334a7751fb72afd53d91c1062606bb766b9e99
ARK <theonlypurplecat@gmail.com>
2021-11-19 20:05:00 +1300

    basic minimap in Alex's Scene

---
+++ Hook&Slice/Assets/Scripts/Enemies/TestDamage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDamage : MonoBehaviour
+{
+    private SpriteRenderer mat;
+
+    private void OnEnable()
+    {
+        mat = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.N))
+        {
+            TakeDamage();
+        }
+    }
+
+    IEnumerator TakeDamageColor()
+    {
+        yield return null;
+    }
+
+    private void TakeDamage()
+    {
+        mat.material.SetColor("_EmissionColor", Color.grey);
+    }
+}
