160a4a311551b34dd819541f96c10a4b1953cb5e
ARK <theonlypurplecat@gmail.com>
2021-11-20 15:08:30 +1300

    adding Minimap, testing Line rendering, and adding GameAnalytics stuff

---
+++ Hook&Slice/Assets/Scripts/Weapons/Chain/LineController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineController : MonoBehaviour
+{
+
+    private LineRenderer lr;
+
+    [SerializeField]
+    private Transform[] points;
+
+    private Harpoon harpoon;
+    private List<Vector3> bouncepoints = new List<Vector3>();
+
+    //private GameObject firePoint;
+
+    private void Start()
+    {
+        //bouncepoints.AddRange(harpoon.hitPoints);
+    }
+
+    private void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+        harpoon = FindObjectOfType<Harpoon>();
+        SetUpLine(points);
+        //firePoint = GameObject.Find("FirePoint");
+        //Vector3 firePos = firePoint.transform.position;
+    }
+
+
+    public void SetUpLine(Transform[] points)
+    {
+        lr.positionCount = points.Length;
+        this.points = points;
+
+
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < points.Length; i++)
+        {
+            lr.SetPosition(i, points[i].position);
+        }
+    }
+
+
+
+}
